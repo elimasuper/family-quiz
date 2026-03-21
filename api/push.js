151d96d9d60ace1d0b65a17b55a083886d5d03cf
elimasuper <eliavmas@yahoo.com>
2026-03-21 19:42:54 +0200

    Update push.js

--- api/push.js
+++ api/push.js
@@ -1,5 +1,7 @@
 import webpush from "web-push";
 
+export const config = { runtime: "nodejs" };
+
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
